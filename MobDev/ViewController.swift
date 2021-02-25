6a64bc47d85b003629d971e6ab3673400f8d9448
Kyrylo Starshynov <star301101@gmail.com>
2021-02-25 09:33:50 +0200

    Lab 1.1

---
+++ MobDev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobDev
+//
+//  Created by Кирило Старшинов on 25.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
